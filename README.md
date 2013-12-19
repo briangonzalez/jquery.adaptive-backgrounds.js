df6a002fab3f3ba14c2c4f763af36e62a2dc5fa3
Brian Gonzalez <me@briangonzalez.org>
2013-12-18 22:38:07 -0800

    issue #4 - cleaned up options and readme

--- README.md
+++ README.md
@@ -35,9 +35,8 @@ Default Options
 ----------------
 - __selector__ String _(default: `'img[data-adaptive-background="1"]'`)_ a CSS selector which denotes which images to grab/process. Ideally, this selector would start with _img_, to ensure we only grab and try to process actual images.
 - __parent__ falsy _(default: `null`)_ a CSS selector which denotes which parent to apply the background color to. By default, the color is applied to the parent one level up the DOM tree.
-- __changeTxtColor__ boolean _(default: `false`)_ option to change color of parent text according generated background image
-- __parentTxtLight__ String _(default: `#fff`)_ text color used when background color is dark
-- __parentTxtDark__ String _(default: `#000`)_ text color used when background color is light 
+- __normalizeTextColor__ boolean _(default: `false`)_ option to normalize the color of the parent text if background color is too dark or too light
+- __normalizedTextColors__ Object Literal _(default: `{dark: '#000', light: '#fff'}`)_ text colors used when background is either too dark/light
 
 
 __Example:__
--- src/jquery.adaptive-backgrounds.js
+++ src/jquery.adaptive-backgrounds.js
@@ -9,17 +9,19 @@
   var EVENT_CF      = 'ab-color-found';
 
   var DEFAULTS      = {
-    selector:   'img[data-adaptive-background="1"]',
-    parent:     null,
-    changeTxtColor: false,
-    parentTxtLight: "#fff",
-    parentTxtDark: "#000",
+    selector:             'img[data-adaptive-background="1"]',
+    parent:               null,
+    normalizeTextColor:   false,
+    normalizedTextColors:  {
+      light:      "#fff",
+      dark:       "#000"
+    }
   };
 
   // Include RGBaster - https://github.com/briangonzalez/rgbaster.js
   /* jshint ignore:start */
   (function(n,t){var a=function(){return document.createElement("canvas").getContext("2d")};var e=function(n,t){var e=new Image;e.crossOrigin="Anonymous";e.src=n.src;e.onload=function(){var r=a();r.drawImage(e,0,0);var o=r.getImageData(0,0,n.width,n.height);t&&t(o.data)}};var r=function(n){return["rgb(",n,")"].join("")};var o=function(n){return n.map(function(n){return r(n.name)})};var i=5;var u=10;var c={};c.colors=function(n,t,a){e(n,function(n){var e=n.length,c={},m="",f=[],l={dominant:{name:"",count:0},palette:Array.apply(null,Array(a||u)).map(Boolean).map(function(n){return{name:"0,0,0",count:0}})};var v=0;while(v<e){f[0]=n[v];f[1]=n[v+1];f[2]=n[v+2];m=f.join(",");if(m in c){c[m]=c[m]+1}else{c[m]=1}if(m!=="0,0,0"&&m!=="255,255,255"){var d=c[m];if(d>l.dominant.count){l.dominant.name=m;l.dominant.count=d}else{l.palette.some(function(n){if(d>n.count){n.name=m;n.count=d;return true}})}}v+=i*4}t&&t({dominant:r(l.dominant.name),palette:o(l.palette)})})};n.RGBaster=n.RGBaster||c})(window);
-   /* jshint ignore:end */
+  /* jshint ignore:end */
 
   /* Our main function declaration. */ 
   $.adaptiveBackground = {
@@ -57,15 +59,16 @@
 
           $parent.css({ backgroundColor: data.color });
 
-          if (opts.changeTxtColor){ 
-            /* Small helper function for calculating color contrast */
-            var getTxtColor = function (){
+          if ( opts.normalizeTextColor ){ 
+
+            /* Helper function to calculate yiq - http://en.wikipedia.org/wiki/YIQ */
+            var getTextColor = function (){
               var rgb = $data.color.match(/\d+/g);
               var yiq = ((rgb[0]*299)+(rgb[1]*587)+(rgb[2]*114))/1000;
-              return yiq >= 128 ? opts.parentTxtDark : opts.parentTxtLight;
+              return yiq >= 128 ? opts.normalizedTextColors.dark : opts.normalizedTextColors.light;
             };
 
-            $parent.css({ color: getTxtColor });
+            $parent.css({ color: getTextColor() });
           }
         });
 
