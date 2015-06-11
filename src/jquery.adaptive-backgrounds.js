91a6f76979f1b1c67ec3651b3d2ce11f68997390
Adam Sivins <adamsiv@microsoft.com>
2015-06-10 17:44:33 -0700

    Ability to adjust the shade of the color based on a % or a blend of a desired color variable. Works in both +/- for light and dark colors.

--- src/jquery.adaptive-backgrounds.js
+++ src/jquery.adaptive-backgrounds.js
@@ -1,128 +1,154 @@
 
 /* jshint debug: true, expr: true */
 
-;(function($){
+;(function($) {
 
-  /* Constants & defaults. */
-  var DATA_COLOR    = 'data-ab-color';
-  var DATA_PARENT   = 'data-ab-parent';
-  var DATA_CSS_BG   = 'data-ab-css-background';
-  var EVENT_CF      = 'ab-color-found';
+    /* Constants & defaults. */
+    var DATA_COLOR      = 'data-ab-color';
+    var DATA_PARENT     = 'data-ab-parent';
+    var DATA_CSS_BG     = 'data-ab-css-background';
+    var EVENT_CF        = 'ab-color-found';
+    var blend           = 'blend';
 
-  var DEFAULTS      = {
-    selector:             '[data-adaptive-background="1"]',
-    parent:               null,
-    exclude:              [ 'rgb(0,0,0)', 'rgba(255,255,255)' ],
-    normalizeTextColor:   false,
-    normalizedTextColors:  {
-      light:      "#fff",
-      dark:       "#000"
-    },
-    lumaClasses:  {
-      light:      "ab-light",
-      dark:       "ab-dark"
+    var DEFAULTS        =   {
+        selector:               '[data-adaptive-background="1"]',
+        parent:                 null,
+        exclude:                [ 'rgb(0,0,0)', 'rgba(255,255,255)' ],
+        normalizeTextColor:     false,
+        normalizedTextColors:   {
+                                    light:      "#fff",
+                                    dark:       "#000" },
+        lumaClasses:  {
+                                    light:      "ab-light",
+                                    dark:       "ab-dark" },
+        shadeVariation:         false,
+        shadePercentage:        0,
+        shadeColors:            {
+                                    light:      'rgb(255,255,255)',
+                                    dark:       'rgb(0,0,0)' }
+    };
+
+    // Include RGBaster - https://github.com/briangonzalez/rgbaster.js
+    /* jshint ignore:start */
+    !function(n){"use strict";var t=function(){return document.createElement("canvas").getContext("2d")},e=function(n,e){var a=new Image,o=n.src||n;"data:"!==o.substring(0,5)&&(a.crossOrigin="Anonymous"),a.onload=function(){var n=t("2d");n.drawImage(a,0,0);var o=n.getImageData(0,0,a.width,a.height);e&&e(o.data)},a.src=o},a=function(n){return["rgb(",n,")"].join("")},o=function(n){return n.map(function(n){return a(n.name)})},r=5,i=10,c={};c.colors=function(n,t){t=t||{};var c=t.exclude||[],u=t.paletteSize||i;e(n,function(e){for(var i=n.width*n.height||e.length,m={},s="",d=[],f={dominant:{name:"",count:0},palette:Array.apply(null,new Array(u)).map(Boolean).map(function(){return{name:"0,0,0",count:0}})},l=0;i>l;){if(d[0]=e[l],d[1]=e[l+1],d[2]=e[l+2],s=d.join(","),m[s]=s in m?m[s]+1:1,-1===c.indexOf(a(s))){var g=m[s];g>f.dominant.count?(f.dominant.name=s,f.dominant.count=g):f.palette.some(function(n){return g>n.count?(n.name=s,n.count=g,!0):void 0})}l+=4*r}if(t.success){var p=o(f.palette);t.success({dominant:a(f.dominant.name),secondary:p[0],palette:p})}})},n.RGBaster=n.RGBaster||c}(window);
+    /* jshint ignore:end */
+
+    function shadeRGBColor(color, percent) { //Found color adjustment
+        var f=color.split(","),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=parseInt(f[0].slice(4)),G=parseInt(f[1]),B=parseInt(f[2]);
+        return "rgb("+(Math.round((t-R)*p)+R)+","+(Math.round((t-G)*p)+G)+","+(Math.round((t-B)*p)+B)+")";
     }
-  };
 
-  // Include RGBaster - https://github.com/briangonzalez/rgbaster.js
-  /* jshint ignore:start */
-  !function(n){"use strict";var t=function(){return document.createElement("canvas").getContext("2d")},e=function(n,e){var a=new Image,o=n.src||n;"data:"!==o.substring(0,5)&&(a.crossOrigin="Anonymous"),a.onload=function(){var n=t("2d");n.drawImage(a,0,0);var o=n.getImageData(0,0,a.width,a.height);e&&e(o.data)},a.src=o},a=function(n){return["rgb(",n,")"].join("")},o=function(n){return n.map(function(n){return a(n.name)})},r=5,i=10,c={};c.colors=function(n,t){t=t||{};var c=t.exclude||[],u=t.paletteSize||i;e(n,function(e){for(var i=n.width*n.height||e.length,m={},s="",d=[],f={dominant:{name:"",count:0},palette:Array.apply(null,new Array(u)).map(Boolean).map(function(){return{name:"0,0,0",count:0}})},l=0;i>l;){if(d[0]=e[l],d[1]=e[l+1],d[2]=e[l+2],s=d.join(","),m[s]=s in m?m[s]+1:1,-1===c.indexOf(a(s))){var g=m[s];g>f.dominant.count?(f.dominant.name=s,f.dominant.count=g):f.palette.some(function(n){return g>n.count?(n.name=s,n.count=g,!0):void 0})}l+=4*r}if(t.success){var p=o(f.palette);t.success({dominant:a(f.dominant.name),secondary:p[0],palette:p})}})},n.RGBaster=n.RGBaster||c}(window);
-  /* jshint ignore:end */
+    function blendRGBColors(c0, c1, p) { //Found color adjustment
+        var f=c0.split(","),t=c1.split(","),R=parseInt(f[0].slice(4)),G=parseInt(f[1]),B=parseInt(f[2]);
+        return "rgb("+(Math.round((parseInt(t[0].slice(4))-R)*p)+R)+","+(Math.round((parseInt(t[1])-G)*p)+G)+","+(Math.round((parseInt(t[2])-B)*p)+B)+")";
+    }
 
-
-  /*
+    /*
     Our main function declaration.
-  */
-  $.adaptiveBackground = {
-    run: function( options ){
-      var opts = $.extend({}, DEFAULTS, options);
+    */
+    $.adaptiveBackground = {
+        run: function( options ) {
+            var opts = $.extend({}, DEFAULTS, options);
 
-      /* Loop over each element, waiting for it to load
-         then finding its color, and triggering the
-         color found event when color has been found.
-      */
-      $( opts.selector ).each(function(index, el){
-        var $this = $(this);
+            /* Loop over each element, waiting for it to load
+             then finding its color, and triggering the
+             color found event when color has been found.
+            */
+            $( opts.selector ).each(function(index, el){
+                var $this = $(this);
 
-        /*  Small helper functions which applies
-            colors, attrs, triggers events, etc.
-        */
-        var handleColors = function () {
-          var img = useCSSBackground() ? getCSSBackground() : $this[0];
+                /*  Small helper functions which applies
+                    colors, attrs, triggers events, etc.
+                */
+                var handleColors = function () {
+                    var img = useCSSBackground() ? getCSSBackground() : $this[0];
 
-          RGBaster.colors(img, {
-            paletteSize: 20,
-            exclude: opts.exclude,
-            success: function(colors) {
-              $this.attr(DATA_COLOR, colors.dominant);
-              $this.trigger(EVENT_CF, { color: colors.dominant, palette: colors.palette });
-            }
-          });
+                    RGBaster.colors(img, {
+                        paletteSize: 20,
+                        exclude: opts.exclude,
+                        success: function(colors) {
+                            $this.attr(DATA_COLOR, colors.dominant);
+                            $this.trigger(EVENT_CF, { color: colors.dominant, palette: colors.palette });
+                        }
+                    });
 
-        };
+                };
 
-        var useCSSBackground = function(){
-          return $this.attr( DATA_CSS_BG );
-        };
+                var useCSSBackground = function(){
+                    return $this.attr( DATA_CSS_BG );
+                };
 
-        var getCSSBackground = function(){
-          return $this.css('background-image')
-                      .replace('url(','').replace(')','');
-        };
+                var getCSSBackground = function(){
+                    return $this.css('background-image')
+                        .replace('url(','').replace(')','');
+                };
 
-        /* Subscribe to our color-found event. */
-        $this.on( EVENT_CF, function(ev, data){
+                /* Subscribe to our color-found event. */
+                $this.on( EVENT_CF, function(ev, data) {
+                    // Helper function to calculate yiq - http://en.wikipedia.org/wiki/YIQ
+                    var getYIQ = function(color){
+                        var rgb = data.color.match(/\d+/g);
+                        return ((rgb[0]*299)+(rgb[1]*587)+(rgb[2]*114))/1000;
+                    };
 
-          // Try to find the parent.
-          var $parent;
-          if ( opts.parent && $this.parents( opts.parent ).length ) {
-            $parent = $this.parents( opts.parent );
-          }
-          else if ( $this.attr( DATA_PARENT ) && $this.parents( $this.attr( DATA_PARENT ) ).length ){
-            $parent = $this.parents( $this.attr( DATA_PARENT ) );
-          }
-          else if ( useCSSBackground() ){
-            $parent = $this;
-          }
-          else if (opts.parent) {
-            $parent = $this.parents( opts.parent );
-          }
-          else {
-            $parent = $this.parent();
-          }
+                    var getNormalizedTextColor = function (color){
+                        return getYIQ(color) >= 128 ? opts.normalizedTextColors.dark : opts.normalizedTextColors.light;
+                    };
 
-          $parent.css({ backgroundColor: data.color });
+                    var getLumaClass = function (color) {
+                        return getYIQ(color) <= 128 ? opts.lumaClasses.dark : opts.lumaClasses.light;
+                    };
 
-          // Helper function to calculate yiq - http://en.wikipedia.org/wiki/YIQ
-          var getYIQ = function(color){
-            var rgb = data.color.match(/\d+/g);
-            return ((rgb[0]*299)+(rgb[1]*587)+(rgb[2]*114))/1000;
-          };
+                    var shadeAdjustment = function ( color ) {
+                        if( opts.shadeVariation == true ) {
+                            return c = getYIQ(color) <= 128 ? shadeRGBColor( color, opts.shadePercentage ) : shadeRGBColor( color, -(opts.shadePercentage), opts.shadePercentage );
+                        }
+                        else if ( opts.shadeVariation == blend ) {
+                            return c = getYIQ(color) >= 128 ? blendRGBColors( color, opts.shadeColors.dark, opts.shadePercentage ) : blendRGBColors( color, opts.shadeColors.light, opts.shadePercentage );
+                        }
+                    };
 
-          var getNormalizedTextColor = function (color){
-            return getYIQ(color) >= 128 ? opts.normalizedTextColors.dark : opts.normalizedTextColors.light;
-          };
+                    // Try to find the parent.
+                    var $parent;
+                    if ( opts.parent && $this.parents( opts.parent ).length ) {
+                        $parent = $this.parents( opts.parent );
+                    }
+                    else if ( $this.attr( DATA_PARENT ) && $this.parents( $this.attr( DATA_PARENT ) ).length ){
+                        $parent = $this.parents( $this.attr( DATA_PARENT ) );
+                    }
+                    else if ( useCSSBackground() ){
+                        $parent = $this;
+                    }
+                    else if (opts.parent) {
+                        $parent = $this.parents( opts.parent );
+                    }
+                    else {
+                        $parent = $this.parent();
+                    }
 
-          var getLumaClass = function (color){
-            return getYIQ(color) <= 128 ? opts.lumaClasses.dark : opts.lumaClasses.light;
-          };
+                    // Check if shade vaiation is requested, and make the adjustment if requested. 
+                    if( opts.shadeVariation != false ) {
+                        data.color = shadeAdjustment( data.color );
+                    }
 
-          // Normalize the text color based on luminance.
-          if ( opts.normalizeTextColor )
-            $parent.css({ color: getNormalizedTextColor(data.color) });
+                    $parent.css({ backgroundColor: data.color });
 
-          // Add a class based on luminance.
-          $parent.addClass( getLumaClass(data.color) )
-                 .attr('data-ab-yaq', getYIQ(data.color));
+                    // Normalize the text color based on luminance.
+                    if ( opts.normalizeTextColor )
+                        $parent.css({ color: getNormalizedTextColor(data.color) });
 
-          opts.success && opts.success($this, data);
-        });
+                    // Add a class based on luminance.
+                    $parent.addClass( getLumaClass(data.color) )
+                        .attr('data-ab-yaq', getYIQ(data.color));
 
-        /* Handle the colors. */
-        handleColors();
+                    opts.success && opts.success($this, data);
+                });
 
-      });
-    },
-  };
+                /* Handle the colors. */
+                handleColors();
+
+            });
+        }
+    };
 
 })(jQuery);
